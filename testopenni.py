0cfd56e731b301b51e20fc27ea42158c69596fcc
Even <even@EvendeMacBook-Pro.local>
2022-04-06 20:16:50 +0100

    1

--- testopenni.py
+++ testopenni.py
@@ -1,20 +1,20 @@
 import cv2
 
 
-capture = cv2.VideoCapture(cv2.CV_CAP_OPENNI)
-capture.set(cv2.CV_CAP_OPENNI_IMAGE_GENERATOR_OUTPUT_MODE, cv2.CV_CAP_OPENNI_VGA_30HZ)
+capture = cv2.VideoCapture(cv2.CAP_OPENNI2
+capture.set(cv2.CAP_OPENNI_IMAGE_GENERATOR_OUTPUT_MODE, cv2.CAP_OPENNI_VGA_30HZ)
 
-print(capture.get(cv2.CV_CAP_PROP_OPENNI_REGISTRATION))
+print(capture.get(cv2.CAP_PROP_OPENNI_REGISTRATION))
 
 while True:
     if not capture.grab():
         print("Unable to Grab Frames from camera")
         break
-    okay1, depth_map = capture.retrieve(cv2.CV_CAP_OPENNI_DEPTH_MAP)
+    okay1, depth_map = capture.retrieve(cv2.CAP_OPENNI_DEPTH_MAP)
     if not okay1:
         print("Unable to Retrieve Disparity Map from camera")
         break
-    okay2, gray_image = capture.retrieve(cv2.CV_CAP_OPENNI_GRAY_IMAGE)
+    okay2, gray_image = capture.retrieve(cv2.CAP_OPENNI_GRAY_IMAGE)
     if not okay2:
         print("Unable to retrieve Gray Image from device")
         break
